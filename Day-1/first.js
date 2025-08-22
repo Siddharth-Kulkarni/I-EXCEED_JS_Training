5a16800cd786227bce7917bfda9aa51d9e497559
Siddharth-Kulkarni <120244251+Siddharth-Kulkarni@users.noreply.github.com>
2025-08-22 16:52:12 +0530

    Add files via upload

---
+++ Day-1/first.js
@@ -0,0 +1,11 @@
+function display()
+{
+     var x=15;
+    // if(true)
+    // {
+    //    console.log(x);
+    // }
+    console.log(x);
+   
+}
+display();
---
+++ Day-1/str1.js
@@ -0,0 +1,31 @@
+let str="hi";
+console.log(str.length);
+
+
+let str1="helllo_ world";
+let str2=str1.toUpperCase();
+
+console.log(str2);
+
+//Extracting Parts
+
+console.log(str2.slice(0,8));
+
+console.log(str2.substring(0,8));
+
+
+//Searching 
+
+
+console.log(str2.indexOf("HELLLO"));
+
+console.log(str2.indexOf("_"));
+
+console.log(str2.lastIndexOf("L"));
+
+
+
+
+
+
+
